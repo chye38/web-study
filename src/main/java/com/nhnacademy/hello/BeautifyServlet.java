f338bc2d04b158c6553f4780e0b3d4de38caf5f8
chye38 <dlrbgud38@gmail.com>
2025-09-15 17:34:38 +0900

    servlet 1일차

---
+++ src/main/java/com/nhnacademy/hello/BeautifyServlet.java
@@ -0,0 +1,28 @@
+package com.nhnacademy.hello;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+
+@Slf4j
+public class BeautifyServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        String html = req.getParameter("html");
+        resp.setContentType("text/plain");
+        resp.setCharacterEncoding("utf-8");
+
+        try (PrintWriter out = resp.getWriter()) {
+            out.println(Jsoup.parse(html));
+        } catch (Exception ex) {
+            log.info(ex.getMessage());
+        }
+    }
+}
---
+++ src/main/java/com/nhnacademy/hello/SetCookieServlet.java
@@ -0,0 +1,28 @@
+package com.nhnacademy.hello;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Objects;
+
+public class SetCookieServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String locale = req.getParameter("locale");
+
+        if(Objects.isNull(locale)){
+            locale = "ko";
+        }
+
+        Cookie cookie = new Cookie("locale",locale);
+        cookie.setMaxAge(-1);
+        cookie.setPath("/");
+        resp.addCookie(cookie);
+
+        try(PrintWriter out = resp.getWriter()){
+            out.println("OK");
+        }
+    }
+}
